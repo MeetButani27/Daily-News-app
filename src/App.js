ad196603ec34264db2f9a235d545d4e04488a1c1
MeetButani27 <20cs010@charusat.edu.in>
2023-03-04 22:53:30 +0530

    Added top loading bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
 
   apiKey = process.env.REACT_APP_NEWS_API_KEY
   pageSize = '6';
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress});
+  }
 
   render() {
     return (
@@ -15,15 +23,21 @@ export default class App extends Component {
         <Router>
           <Navbar />
 
+          <LoadingBar
+            color='#faac37'
+            progress = {this.state.progress}
+            height = {3}
+          />
+
           <Routes>
-            <Route exact path='/' element={<News key='home' pageSize={this.pageSize} country='in' category='general' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/business' element={<News key='business' pageSize={this.pageSize} country='in' category='business' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/general' element={<News key='general' pageSize={this.pageSize} country='in' category='general' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/health' element={<News key='health' pageSize={this.pageSize} country='in' category='health' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/science' element={<News key='science' pageSize={this.pageSize} country='in' category='science' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/sports' element={<News key='sports' pageSize={this.pageSize} country='in' category='sports' apiKey={this.apiKey} />}> </Route>
-            <Route exact path='/technology' element={<News key='technology' pageSize={this.pageSize} country='in' category='technology' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/' element={<News setProgress={this.setProgress} key='home' pageSize={this.pageSize} country='in' category='general' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/business' element={<News setProgress={this.setProgress} key='business' pageSize={this.pageSize} country='in' category='business' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/entertainment' element={<News setProgress={this.setProgress} key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/general' element={<News setProgress={this.setProgress} key='general' pageSize={this.pageSize} country='in' category='general' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/health' element={<News setProgress={this.setProgress} key='health' pageSize={this.pageSize} country='in' category='health' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/science' element={<News setProgress={this.setProgress} key='science' pageSize={this.pageSize} country='in' category='science' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/sports' element={<News setProgress={this.setProgress} key='sports' pageSize={this.pageSize} country='in' category='sports' apiKey={this.apiKey} />}> </Route>
+            <Route exact path='/technology' element={<News setProgress={this.setProgress} key='technology' pageSize={this.pageSize} country='in' category='technology' apiKey={this.apiKey} />}> </Route>
           </Routes>
         </Router>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,9 @@ export class NewsItem extends Component {
     return (
         <div className="m-3">
             <div className="card" >
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex: '1'}}>{ source }</span>
+                <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right:'0'}}>
+                    <span className="badge rounded-pill bg-danger">{ source }</span>
+                </div>
 
                 <img src={!imageUrl ? "https://moz.com/images/cms/6042b901d424f8.96940036_2021-04-15-230656.png?w=1200&h=630&q=82&auto=format&fit=crop&dm=1618528016&s=b298c4d810e8e8ad23b1fc18e845f64e":imageUrl } className="card-img-top" alt="..." />
                 <div className="card-body">
